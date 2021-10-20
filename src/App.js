c38b1e2a8d6b37a55a9c6fc63d46e69f5e6ee73d
Hernán D. Mercado <hernandmf@gmail.com>
2021-10-20 16:35:13 -0500

    Logo component

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from "react";
 import { GlobalStyle } from "./GlobalStyle";
 import { ListOfCategories } from "./components/ListOfCategories";
 import { ListPhotoCart } from "./components/ListPhotoCart";
+import { Logo } from "./components/Logo";
 
 const App = () => {
   return (
     <div>
+      <Logo />
       <GlobalStyle />
       <ListOfCategories />
       <ListPhotoCart />
