02b6a9a32a918da8bd6a34ec4302315e88553561
Hernán D. Mercado <hernandmf@gmail.com>
2021-10-21 18:30:25 -0500

    Initializing react apollo client and mi first hoc component

--- src/components/ListPhotoCart/index.js
+++ src/components/ListPhotoCart/index.js
@@ -1,10 +1,37 @@
+//DEPENDENCIES
 import React from "react";
+import { useQuery, gql } from "@apollo/client";
+
+//COMPONENTS
 import { PhotoCart } from "../PhotoCart";
+
+const whitPhotos = gql`
+  query getPhotos {
+    photos {
+      id
+      categoryId
+      src
+      likes
+      userId
+      liked
+    }
+  }
+`;
+
 export const ListPhotoCart = () => {
+  const { loading, error, data } = useQuery(whitPhotos);
+
+  if (error) {
+    return <h2>Internal Server Error</h2>;
+  }
+  if (loading) {
+    return <h2>Loading...</h2>;
+  }
+
   return (
     <ul>
-      {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((id) => (
-        <PhotoCart key={id} id={id} />
+      {data.photos.map((photo) => (
+        <PhotoCart key={photo.id} {...photo} />
       ))}
     </ul>
   );
--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
+//DEPENDENCIES
 import React from "react";
 import ReactDOM from "react-dom";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+
+//APP
 import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("App"));
+const client = new ApolloClient({
+  uri: "https://petgram-server-clgg.vercel.app/graphql",
+  cache: new InMemoryCache(),
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+
+  document.getElementById("App")
+);
