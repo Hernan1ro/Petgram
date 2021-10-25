88b6fb0c20fac4e2f4bfd658065158f6b262e331
Hernán D. Mercado <hernandmf@gmail.com>
2021-10-25 14:11:15 -0500

    Secured routes

--- src/App.js
+++ src/App.js
@@ -5,6 +5,13 @@ import { NavBar } from "./components/NavBar";
 import Home from "./pages/Home";
 import { Detail } from "./pages/Detail";
 import { Route, Switch, BrowserRouter } from "react-router-dom";
+import { Favs } from "./pages/Favs";
+import { User } from "./pages/User";
+import { NotRegistered } from "./pages/NotRegistered";
+
+const UserLogged = ({ children }) => {
+  return children({ isAuth: false });
+};
 
 const App = () => {
   return (
@@ -19,6 +26,21 @@ const App = () => {
         />
         <Route exact path="/home" render={(props) => <Home {...props} />} />
         <Route exact path="/pet/:id" render={(props) => <Home {...props} />} />
+        <UserLogged>
+          {({ isAuth }) =>
+            isAuth ? (
+              <Switch>
+                <Route exact path="/user" component={User} />
+                <Route exact path="/favs" component={Favs} />
+              </Switch>
+            ) : (
+              <Switch>
+                <Route exact path="/favs" component={NotRegistered} />
+                <Route exact path="/user" component={NotRegistered} />
+              </Switch>
+            )
+          }
+        </UserLogged>
       </Switch>
       <NavBar />
     </BrowserRouter>
---
+++ src/pages/NotRegistered.js
@@ -0,0 +1,3 @@
+import React from "react";
+
+export const NotRegistered = () => <h1>Not registered</h1>;
