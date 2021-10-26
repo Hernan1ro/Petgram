f5363340494c3afc8a0d3ecbc606d9d44472d0f8
Hernán D. Mercado <hernandmf@gmail.com>
2021-10-26 12:53:58 -0500

    Mutations to sing in

--- src/pages/NotRegistered.js
+++ src/pages/NotRegistered.js
@@ -2,13 +2,25 @@ import React from "react";
 import { UserForm } from "../components/UserForm";
 import Context from "../context/context";
 import { useRegisterMutation } from "../container/RegisterMutation";
+import { useLoginMutation } from "../container/LoginMutation";
 
 export const NotRegistered = () => {
-  const { registerMutation, data, loading, error } = useRegisterMutation();
+  const {
+    registerMutation,
+    data,
+    loading: loadingRegister,
+    error: errorRegister,
+  } = useRegisterMutation();
+  const {
+    login,
+    loading: loadingLogin,
+    error: errorLogin,
+  } = useLoginMutation();
 
   const errorMsg =
-    error && "El usuario ya se encuentra registrado o hubo un problema";
-
+    errorRegister && "El usuario ya se encuentra registrado o hubo un problema";
+  const errorMsgLogin =
+    errorLogin && "Contraseña incorrecta, usuario incorrecto o no existente";
   return (
     <Context.Consumer>
       {({ activateAuth }) => {
@@ -17,15 +29,31 @@ export const NotRegistered = () => {
           const variables = { input };
           registerMutation({ variables }).then(activateAuth);
         };
+        const onSubmitLogin = ({ email, password }) => {
+          const input = { email, password };
+          const variables = { input };
+          login({ variables })
+            .then((res) => {
+              activateAuth();
+            })
+            .catch((err) => {
+              console.log(err);
+            });
+        };
         return (
           <>
             <UserForm
               error={errorMsg}
-              loading={loading}
+              loading={loadingRegister}
               onSubmit={onSubmit}
               title="Registrarse"
             />
-            <UserForm onSubmit={activateAuth} title="Iniciar Sesion" />
+            <UserForm
+              loading={loadingLogin}
+              error={errorMsgLogin}
+              onSubmit={onSubmitLogin}
+              title="Iniciar Sesion"
+            />
           </>
         );
       }}
