11d7444737d74c9e002efc0e891a644c65abd04b
Hernán D. Mercado <hernandmf@gmail.com>
2021-10-27 13:25:42 -0500

    last adjustemts to the routes

--- src/App.js
+++ src/App.js
@@ -4,10 +4,11 @@ import { Logo } from "./components/Logo";
 import { NavBar } from "./components/NavBar";
 import Home from "./pages/Home";
 import { Detail } from "./pages/Detail";
-import { Route, Switch, BrowserRouter } from "react-router-dom";
+import { Route, Switch, BrowserRouter, Redirect } from "react-router-dom";
 import { Favs } from "./pages/Favs";
 import { User } from "./pages/User";
 import { NotRegistered } from "./pages/NotRegistered";
+import { NotFound } from "./components/NotFound";
 import Context from "./context/context";
 
 const App = () => {
@@ -16,27 +17,35 @@ const App = () => {
       <GlobalStyle />
       <Logo />
       <Switch>
-        <Route
-          exact
-          path="/detail/:id"
-          render={(props) => <Detail {...props} />}
-        />
-        <Route exact path="/home" render={(props) => <Home {...props} />} />
-        <Route exact path="/pet/:id" render={(props) => <Home {...props} />} />
         <Context.Consumer>
-          {({ isAuth }) =>
-            isAuth ? (
-              <Switch>
-                <Route exact path="/user" component={User} />
-                <Route exact path="/favs" component={Favs} />
-              </Switch>
-            ) : (
-              <Switch>
-                <Route exact path="/favs" component={NotRegistered} />
-                <Route exact path="/user" component={NotRegistered} />
-              </Switch>
-            )
-          }
+          {({ isAuth }) => (
+            <Switch>
+              <Route
+                exact
+                path="/detail/:id"
+                render={(props) => <Detail {...props} />}
+              />
+              <Route
+                exact
+                path="/home"
+                render={(props) => <Home {...props} />}
+              />
+              <Route
+                exact
+                path="/pet/:id"
+                render={(props) => <Home {...props} />}
+              />
+              {!isAuth && (
+                <Route exact path="/login" component={NotRegistered} />
+              )}
+              {!isAuth && <Redirect from="/favs" to="/login" />}
+              {!isAuth && <Redirect from="/user" to="/login" />}
+              {isAuth && <Redirect from="/login" to="/home" />}
+              <Route exact path="/user" component={User} />
+              <Route exact path="/favs" component={Favs} />
+              <Route path="*" component={NotFound} />
+            </Switch>
+          )}
         </Context.Consumer>
       </Switch>
       <NavBar />
