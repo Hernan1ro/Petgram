663153fb7bb2ad8db4ce8f5153a055d0c1e7849a
Hernán D. Mercado <hernandmf@gmail.com>
2021-10-28 17:47:22 -0500

    [fix] some stuff

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { GlobalStyle } from "./styles/GlobalStyle";
-import { Logo } from "./components/Logo";
+import { Logo } from "./components/logo";
 import { NavBar } from "./components/NavBar";
 import Home from "./pages/Home";
 import { Detail } from "./pages/Detail";
